eda752db9eb261bcaa2a186299a26610b5c36cdb
jlacy9554 <66134768+jlacy9554@users.noreply.github.com>
2021-04-06 19:28:59 -0400

    handling db filling

--- metube/media_upload_process.php
+++ metube/media_upload_process.php
@@ -43,14 +43,44 @@ if(!file_exists($dirfile))
 							  mediaid, filename,filepath,type)".
 							  "values(NULL,'". urlencode($_FILES["file"]["name"])."','$dirfile','".$_FILES["file"]["type"]."')";
 					$queryresult = mysqli_query($db->db_connect_id,$insert)
-						  or die("Insert into Media error in media_upload_process.php " .mysql_error());
+						  or die("Insert into Media error in media_upload_process.php " .mysqli_error($db->db_connect_id));
 					$result="0";
 					
-					$mediaid = mysqli_insert_id($db->db_connect_id,);
+					$mediaid = mysqli_insert_id($db->db_connect_id);
 					//insert into upload table
 					$insertUpload="insert into upload(uploadid,username,mediaid) values(NULL,'$username','$mediaid')";
 					$queryresult = mysqli_query($db->db_connect_id,$insertUpload)
-						  or die("Insert into view error in media_upload_process.php " .mysql_error());
+						  or die("Insert into view error in media_upload_process.php " .mysqli_error($db->db_connect_id));
+
+					//setup for video information
+					$title = $_POST['title'];
+					if($title == ''){
+						$title=urlencode($_FILES["file"]["name"]);
+					}
+					$desc = $_POST['desc'];
+					if($desc == ''){
+						$desc = "No description provided";
+					}
+					$str = $_POST['keywords'];
+
+					//add to the mediainfo table
+					
+					
+					$addto ="INSERT INTO mediainfo (mediainfoid,title,description) VALUES ('$mediaid','$title','$desc')";
+					$queryresult = mysqli_query($db->db_connect_id,$addto)
+						  or die("Insert into mediainfo error in media_upload_process.php " .mysqli_error($db->db_connect_id));
+					//add to the keywords table
+					if($str != ''){
+						$words = array();
+						$words = explode(' ', $str);
+						for($i=0;$i<= (count($words) -1);$i++){
+							$word = $words[$i];
+						
+							$addkey="INSERT INTO keywords (id, videoid, keyword) VALUES (NULL,'$mediaid','$word')";
+							$queryresult = mysqli_query($db->db_connect_id,$addkey)
+							or die("Insert into keyword error in media_upload_process.php " .mysqli_error($db->db_connect_id));
+						}
+					}
 				}
 			}
 			else  
