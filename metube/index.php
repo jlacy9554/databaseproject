e60dcce48125a72b06636f8634a2701b86e7d581
jlacy9554 <66134768+jlacy9554@users.noreply.github.com>
2021-04-13 19:37:11 -0400

    added updated browse

--- metube/index.php
+++ metube/index.php
@@ -23,10 +23,8 @@ if(isset($_POST['submit'])) {
 			}		
 		}
 }
-
-
- 
 ?>
+
 	<form method="post" action="<?php echo "index.php"; ?>">
 
 	<table width="100%">
@@ -42,10 +40,10 @@ if(isset($_POST['submit'])) {
         
 			<td><input name="submit" type="submit" value="Login"><input name="reset" type="reset" value="Reset"><br /></td>
 		</tr>
+		<tr>
+			
+		</tr>
 	</table>
-	</form>
-	<form action="logout.php">
-		<input type="submit" value="Logout">
 	</form>
 	<form action="register.php">
 		<input type="submit" value="Register">
@@ -54,3 +52,45 @@ if(isset($_POST['submit'])) {
   if(isset($login_error))
    {  echo "<div id='passwd_result'>".$login_error."</div>";}
 ?>
+<?php
+
+
+$query = "SELECT * from media"; 
+$result = mysqli_query($db->db_connect_id, $query );
+if (!$result)
+{
+   die ("Could not query the media table in the database: <br />". mysqli_error($db->db_connect_id));
+}
+$queryi = "SELECT * from mediainfo";
+$infores = mysqli_query($db->db_connect_id,$queryi);
+?>
+
+<div style="background:#339900;color:#FFFFFF; width:150px;">Uploaded Media</div>
+<table width="50%" cellpadding="0" cellspacing="0">
+	<?php
+		while ($result_row = mysqli_fetch_row($result))
+		{ $resi_row = mysqli_fetch_row($infores);
+	?>
+	<tr valign="top">			
+		<td>
+				<?php 
+					echo $result_row[0];
+				?>
+		</td>
+		<td>
+			<?php
+				echo $resi_row[1];
+			?>
+		</td>
+		<td>
+			<a href="media.php?id=<?php echo $result_row[0];?>" target="_blank"><?php echo $result_row[1];?></a> 
+		</td>
+		<td>
+			<a href="<?php echo $result_row[2].$result_row[1];?>" target="_blank" onclick="javascript:saveDownload(<?php echo $result_row[0];?>);">Download</a>
+		</td>
+	</tr>
+	<?php
+		}
+	?>
+</table>
+
