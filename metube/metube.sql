cb0437c130cc70fb90d944b462cfea228bd90a64
cmearl6 <cmearl@g.clemson.edu>
2021-04-13 21:55:25 -0400

    updated database

--- metube/metube.sql
+++ metube/metube.sql
@@ -1,194 +1,265 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 13, 2021 at 11:03 PM
--- Server version: 8.0.21
--- PHP Version: 7.3.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: mysql1.cs.clemson.edu    Database: MeTubeProject_zfvm
+-- ------------------------------------------------------
+-- Server version	5.5.52-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `metube`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `account`
 --
 
 DROP TABLE IF EXISTS `account`;
-CREATE TABLE IF NOT EXISTS `account` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account` (
   `username` varchar(50) NOT NULL,
   `password` varchar(128) NOT NULL,
   `email` varchar(60) DEFAULT NULL,
-  `id` int NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`username`),
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`username`, `password`, `email`, `id`) VALUES
-('metube', '123456', 'ldong@clemson.edu', 1),
-('test', '1234', 'a@a', 2),
-('test2', '123', 'a@a', 3);
-
--- --------------------------------------------------------
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES ('cme','cme','cme@gmail.com',4),('metube','123456','ldong@clemson.edu',1),('test','1234','a@a',2),('test2','123','a@a',3);
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `comment`
 --
 
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE IF NOT EXISTS `comment` (
-  `cid` int NOT NULL AUTO_INCREMENT,
-  `vidid` int NOT NULL,
-  `userid` int NOT NULL,
-  `comments` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `vidid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `comments` varchar(500) NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`cid`, `vidid`, `userid`, `comments`) VALUES
-(4, 3, 2, 'Comment here'),
-(5, 3, 2, 'Comment here');
-
--- --------------------------------------------------------
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (4,3,2,'Comment here'),(5,3,2,'Comment here');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `download`
 --
 
 DROP TABLE IF EXISTS `download`;
-CREATE TABLE IF NOT EXISTS `download` (
-  `downloadid` int NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `download` (
+  `downloadid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `mediaid` int NOT NULL,
+  `mediaid` int(11) NOT NULL,
   `downloadtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`downloadid`),
   KEY `username` (`username`),
   KEY `mediaid` (`mediaid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `download`
 --
 
-INSERT INTO `download` (`downloadid`, `username`, `mediaid`, `downloadtime`) VALUES
-(1, 'metube', 5, '2008-09-06 16:48:21'),
-(2, 'metube', 4, '2008-09-06 16:49:36'),
-(3, 'metube', 4, '2008-09-06 17:18:44'),
-(4, 'test', 3, '2021-04-05 22:35:57');
-
--- --------------------------------------------------------
+LOCK TABLES `download` WRITE;
+/*!40000 ALTER TABLE `download` DISABLE KEYS */;
+INSERT INTO `download` VALUES (1,'metube',5,'2008-09-06 16:48:21'),(2,'metube',4,'2008-09-06 16:49:36'),(3,'metube',4,'2008-09-06 17:18:44'),(4,'test',3,'2021-04-05 22:35:57');
+/*!40000 ALTER TABLE `download` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `keywords`
 --
 
 DROP TABLE IF EXISTS `keywords`;
-CREATE TABLE IF NOT EXISTS `keywords` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `videoid` int NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `keywords` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `videoid` int(11) NOT NULL,
   `keyword` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `keyid` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `keywords`
+--
+
+LOCK TABLES `keywords` WRITE;
+/*!40000 ALTER TABLE `keywords` DISABLE KEYS */;
+/*!40000 ALTER TABLE `keywords` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `media`
 --
 
 DROP TABLE IF EXISTS `media`;
-CREATE TABLE IF NOT EXISTS `media` (
-  `mediaid` int NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media` (
+  `mediaid` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(64) NOT NULL,
   `filepath` varchar(256) NOT NULL,
   `type` varchar(30) DEFAULT '0',
   `lastaccesstime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`mediaid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `media`
 --
 
-INSERT INTO `media` (`mediaid`, `filename`, `filepath`, `type`, `lastaccesstime`) VALUES
-(3, 'sample2.wmv', 'uploads/metube/', 'video/x-ms-wmv', '2021-04-09 03:52:31'),
-(4, 'sample3.wmv', 'uploads/metube/', 'video/x-ms-wmv', '2021-04-12 23:30:41'),
-(5, 'sample1.wmv', 'uploads/metube/', 'video/x-ms-wmv', '2021-04-08 01:30:46'),
-(9, 'nintendogs_wallcoo.com_6.jpg', 'uploads/metube/', 'image/jpeg', '2021-04-08 00:11:45');
-
--- --------------------------------------------------------
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+INSERT INTO `media` VALUES (3,'sample2.wmv','uploads/metube/','video/x-ms-wmv','2021-04-09 03:52:31'),(4,'sample3.wmv','uploads/metube/','video/x-ms-wmv','2021-04-12 23:30:41'),(5,'sample1.wmv','uploads/metube/','video/x-ms-wmv','2021-04-08 01:30:46'),(9,'nintendogs_wallcoo.com_6.jpg','uploads/metube/','image/jpeg','2021-04-08 00:11:45');
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `mediainfo`
 --
 
 DROP TABLE IF EXISTS `mediainfo`;
-CREATE TABLE IF NOT EXISTS `mediainfo` (
-  `mediainfoid` int NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mediainfo` (
+  `mediainfoid` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
-  `description` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` varchar(5000) NOT NULL,
   PRIMARY KEY (`mediainfoid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `mediainfo`
 --
 
-INSERT INTO `mediainfo` (`mediainfoid`, `title`, `description`) VALUES
-(3, 'sample 2', 'this is sample 2'),
-(4, 'sample 3', 'sample 3 infp'),
-(5, 'sample 1', 'this is sample 1'),
-(9, 'nintendogs', 'this is nintendogs');
+LOCK TABLES `mediainfo` WRITE;
+/*!40000 ALTER TABLE `mediainfo` DISABLE KEYS */;
+INSERT INTO `mediainfo` VALUES (3,'sample 2','this is sample 2'),(4,'sample 3','sample 3 infp'),(5,'sample 1','this is sample 1'),(9,'nintendogs','this is nintendogs');
+/*!40000 ALTER TABLE `mediainfo` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `playlist_media`
+--
+
+DROP TABLE IF EXISTS `playlist_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playlist_media` (
+  `playlistid` int(11) NOT NULL,
+  `mediaid` int(11) NOT NULL,
+  KEY `playlist_idx` (`playlistid`),
+  KEY `media_idx` (`mediaid`),
+  CONSTRAINT `playlist` FOREIGN KEY (`playlistid`) REFERENCES `playlists` (`playlistid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `media` FOREIGN KEY (`mediaid`) REFERENCES `media` (`mediaid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playlist_media`
+--
+
+LOCK TABLES `playlist_media` WRITE;
+/*!40000 ALTER TABLE `playlist_media` DISABLE KEYS */;
+INSERT INTO `playlist_media` VALUES (3,3),(3,3),(3,3),(3,4),(3,5),(3,3),(3,4),(3,5),(3,3),(3,3),(3,4),(3,4),(3,4),(3,4),(3,4),(3,3),(3,3),(3,3),(3,3),(3,4),(3,4),(4,3),(4,4),(4,3),(3,4),(4,3),(4,4),(4,5),(4,5),(4,5),(4,5),(4,5),(2,5),(2,5),(2,4),(10,4),(5,9),(5,5),(5,9),(3,4),(18,4),(18,5),(11,3),(11,3),(11,4),(11,3),(20,4),(22,5),(22,9),(22,5),(22,3),(22,9),(22,5),(23,5),(23,4),(23,9),(25,5),(3,9),(5,4),(21,3),(21,9),(24,5),(28,4),(28,5);
+/*!40000 ALTER TABLE `playlist_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `playlists`
+--
+
+DROP TABLE IF EXISTS `playlists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playlists` (
+  `playlistid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`playlistid`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playlists`
+--
+
+LOCK TABLES `playlists` WRITE;
+/*!40000 ALTER TABLE `playlists` DISABLE KEYS */;
+INSERT INTO `playlists` VALUES (2,'test',1),(3,'new playlist',1),(4,'Cole',1),(5,'coles playlist',1),(10,'funny videos',2),(11,'new',2),(12,'coles videos',2),(13,'new video',2),(18,'test playlist',2),(19,'test playlist',3),(20,'playlist',3),(21,'test playlist',1),(22,'cme\'s playlist',4),(23,'2nd playlist',4),(24,'test playlist',4),(25,'test2',4),(26,'test playlist',2),(27,'test',4),(28,'newest ',4);
+/*!40000 ALTER TABLE `playlists` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `upload`
 --
 
 DROP TABLE IF EXISTS `upload`;
-CREATE TABLE IF NOT EXISTS `upload` (
-  `uploadid` int NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `upload` (
+  `uploadid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `mediaid` int NOT NULL,
+  `mediaid` int(11) NOT NULL,
   `uploadtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`uploadid`),
   KEY `username` (`username`),
   KEY `mediaid` (`mediaid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `upload`
 --
 
-INSERT INTO `upload` (`uploadid`, `username`, `mediaid`, `uploadtime`) VALUES
-(3, 'metube', 3, '2008-09-05 19:52:19'),
-(4, 'metube', 4, '2008-09-05 19:53:10'),
-(5, 'metube', 5, '2008-09-05 19:53:47'),
-(9, 'metube', 9, '2008-09-05 20:28:36');
-COMMIT;
+LOCK TABLES `upload` WRITE;
+/*!40000 ALTER TABLE `upload` DISABLE KEYS */;
+INSERT INTO `upload` VALUES (3,'metube',3,'2008-09-05 19:52:19'),(4,'metube',4,'2008-09-05 19:53:10'),(5,'metube',5,'2008-09-05 19:53:47'),(9,'metube',9,'2008-09-05 20:28:36');
+/*!40000 ALTER TABLE `upload` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-13 21:50:35
